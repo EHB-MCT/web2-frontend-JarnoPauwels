52f97953f59207a44b6fa27b46d2192ab08f774c
JarnoPauwels <71709292+JarnoPauwels@users.noreply.github.com>
2021-12-18 21:47:08 +0100

    Fixed Post

--- src/games.js
+++ src/games.js
@@ -1,4 +1,8 @@
 window.onload = function(){
+
+    document.getElementById('dataContainer').innerHTML = "";
+    document.getElementById('dataContainer').style.display = "block";
+
     async function displayRandomGames(){
         const response = await fetch(`https://api.boardgameatlas.com/api/search?name=?&pretty=true&client_id=zKw4L2EcTf`);
         const game = await response.json();
@@ -20,7 +24,7 @@ window.onload = function(){
                                 <h1>${element.name}</h1>
                             </div>
                             <div class="col">
-                                <h1><a href="" id="linkTo">Add Scores</a></h1>
+                                <h1><a href="" id="linkTo">Add Score</a></h1>
                             </div>
                         </div>
                         <div class="row">
@@ -62,10 +66,12 @@ async function getGames(){
             `   <div class="rowContainer">
                     <div class="row">
                         <div class="col">
-                            <h1>${element.name}</h1>
+                            <h1 id="gameName">${element.name}</h1>
                         </div>
                         <div class="col">
-                            <h2><a href="" id="linkTo">Add Scores</a></h2>
+                            <form id="addGame">         
+                                <button type="submit" onclick="addScore();">Add Score</button>
+                            </form>
                         </div>
                     </div>
                     <div class="row">
@@ -73,41 +79,40 @@ async function getGames(){
                             <img src="${element.image_url}" alt="">
                         </div>  
                         <div class="col">       
-                            <p>Description: ${element.description}</p>
+                            <p id="gameDesc">${element.description}</p>
                         </div>
                     </div>
                 </div>        
             `;
         document.getElementById('dataContainer').innerHTML = htmlString;
-
-//         //`${element.id}`
-//         // let sectionString = "";
-//         // document.getElementById('linkTo').addEventListener("click", (e) => {
-//         //     console.log(e);
-//         //     document.getElementById('dataContainer').innerHTML = "";
-//         //     document.getElementById('section').style.display = "block";
-//         //     sectionString += 
-//         //         `   <div class="rowContainer">
-//         //                 <div class="row">
-//         //                     <div class="col">
-//         //                         <h1>${element.name}</h1>
-//         //                     </div>
-//         //                     <div class="col">
-//         //                         <h1><a href="#section" id="linkTo">See Scores</a></h1>
-//         //                     </div>
-//         //                 </div>
-//         //                 <div class="row">
-//         //                     <div class="col">    
-//         //                         <img src="${element.image_url}" alt="">
-//         //                     </div>  
-//         //                     <div class="col">       
-//         //                         <p>Description: ${element.description}</p>
-//         //                     </div>
-//         //                 </div>
-//         //             </div>        
-//         //         `;
-//         //     document.getElementById('section').innerHTML = sectionString;
-//         // }); 
-
     });
-};
+}
+
+
+async function addScore(){
+    let gameName = document.querySelector("#gameName").innerHTML;
+    let gameDesc = document.querySelector('#gameDesc').parentElement.innerText;
+    console.log(gameName, gameDesc);
+
+        fetch(`https://web2-courseproject-jarno.herokuapp.com/coursedata`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                    user: "dwdddqqqq2",
+                    score: "wwdd",
+                    game: gameName,
+                    desc: gameDesc
+            })
+        }).then(response => {
+            return response.json()
+        }).then(async data => {
+            console.log(data);
+        });
+
+        document.getElementById('addGame').addEventListener('submit', (e) => {
+        e.preventDefault();
+    });
+
+}
